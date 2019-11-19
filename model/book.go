3f61cf87d238951149067fd167816c53f5bf364f
thotsaphonjantree <thotsaphon.jantree@gmail.com>
2019-11-19 10:52:40 +0700

    fix uppercase to lowercase

--- model/book.go
+++ model/book.go
@@ -4,13 +4,11 @@ import "gopkg.in/mgo.v2/bson"
 
 // Book Model
 type Book struct {
-
-	BookID    	bson.ObjectId 	`json:"_id" bson:"_id"`
-	Isbn      	string        	`json:"isbn" bson:"isbn"`
-	Title     	string        	`json:"title" bson:"title"`
-	Author    	[]string      	`json:"author" bson:"Author"`
-	Publisher 	string     	`json:"publisher" bson:"Publisher"`
-	ImgURL    	string        	`json:"imgURL" bson:"imgURL"`
-	Tags		[]bson.ObjectId `json:"tags" bson:"tags"`
-
+	BookID    bson.ObjectId   `json:"_id" bson:"_id"`
+	Isbn      string          `json:"isbn" bson:"isbn"`
+	Title     string          `json:"title" bson:"title"`
+	Author    []string        `json:"author" bson:"author"`
+	Publisher string          `json:"publisher" bson:"publisher"`
+	ImgURL    string          `json:"imgURL" bson:"imgURL"`
+	Tags      []bson.ObjectId `json:"tags" bson:"tags"`
 }
